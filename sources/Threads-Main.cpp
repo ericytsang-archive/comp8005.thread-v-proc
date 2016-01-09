a1c9dab6b3a1eae17576606488cb01650da61288
Eric Tsang <surplus.et@gmail.com>
2016-01-09 00:31:47 -0800

    modified makefile to be able to build a processes version, and threads version, and created the threaded main and processes main
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ sources/Threads-Main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <stdio.h>
+#include "FindFactorsTask.h"
+#include "Number.h"
+
+int main()
+{
+    // create all data structures needed to store results, tasks, and execution
+    // statistics
+    std::vector<FindFactorsTask*> tasks;
+    std::vector<Number*> results;
+    // todo: create a thing used to store execution statistics
+
+    // todo: create the worker threads
+
+
+    return 0;
+}
+
+int worker_routine()
+{
+    // todo
+    //
+    return 0;
+}
