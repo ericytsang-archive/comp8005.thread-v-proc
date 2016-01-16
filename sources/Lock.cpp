8db9ac2b4344899423c2b1164408ae2b9a20a1b0
Eric Tsang <surplus.et@gmail.com>
2016-01-15 16:02:46 -0800

    added comments to the Lock class
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- sources/Lock.cpp
+++ sources/Lock.cpp
@@ -1,10 +1,74 @@
+/**
+ * implementation of the Lock class declared in Lock.h
+ *
+ * @sourceFile Lock.cpp
+ *
+ * @program    Threads-Main.out, Processes-Main.out
+ *
+ * @class      Lock
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * used to wait on, and post to a semaphore using the RAII (Resource Allocation
+ *   Is Initialization) idiom.
+ *
+ * it waits on the semaphore when it is constructed, and when destroyed, posts
+ *   back to the semaphore.
+ */
 #include "Lock.h"
 
+/**
+ * waits upon the passed semaphore object.
+ *
+ * @class      Lock
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note       none
+ *
+ * @signature  Lock::Lock(sem_t* _sem)
+ *
+ * @param      _sem pointer to the semaphore to wait on when constructed, and
+ *   post to once destructed.
+ *
+ * @return     an instance of the Lock class.
+ */
 Lock::Lock(sem_t* _sem):sem(_sem)
 {
     sem_wait(sem);
 }
 
+/**
+ * posts to the semaphore object passed in from the constructor.
+ *
+ * @class      Lock
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note       none
+ *
+ * @signature  ~Lock()
+ */
 Lock::~Lock()
 {
     sem_post(sem);
--- sources/Lock.h
+++ sources/Lock.h
@@ -1,8 +1,27 @@
+/**
+ * header file for the Lock class. implementation is in Lock.cpp
+ *
+ * @sourceFile Lock.h
+ *
+ * @program    Threads-Main.out, Processes-Main.out
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * used to wait on, and post to a semaphore using the RAII (Resource Allocation
+ *   Is Initialization) idiom.
+ */
 #ifndef LOCK_H
 #define LOCK_H
 
 #include "semaphore.h"
-
 class Lock
 {
 public:
