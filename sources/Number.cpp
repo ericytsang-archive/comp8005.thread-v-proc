4fd43d7ec33cc3db34668917250c481ccad1af25
Eric Tsang <surplus.et@gmail.com>
2016-01-15 16:07:07 -0800

    added comments to the number class
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- sources/Number.cpp
+++ sources/Number.cpp
@@ -1,10 +1,77 @@
+/**
+ * implementation of the Number class declared in Number.h
+ *
+ * @sourceFile Number.cpp
+ *
+ * @program    Threads-Main.out, Processes-Main.out
+ *
+ * @class      Number
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * used to wrap an integer of the gmp library so that they will be properly
+ *   initialized and deallocated when an instance of this class is constructed
+ *   or destroyed.
+ */
 #include "Number.h"
 
+/**
+ * instantiates a Number instance.
+ *
+ * @class      Number
+ *
+ * @method     Number
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * initializes the internal gnp integer upon construction.
+ *
+ * @signature  Number()
+ *
+ * @return     an instance of the Number class.
+ */
 Number::Number()
 {
     mpz_init(value);
 }
 
+/**
+ * destroys a Number instance.
+ *
+ * @class      Number
+ *
+ * @method     ~Number
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * clears the internal gnp integer upon destruction.
+ *
+ * @signature  ~Number()
+ */
 Number::~Number()
 {
     mpz_clear(value);
--- sources/Number.h
+++ sources/Number.h
@@ -1,3 +1,24 @@
+/**
+ * header file for the Number class. implementation is in Number.cpp
+ *
+ * @sourceFile Number.h
+ *
+ * @program    Threads-Main.out, Processes-Main.out
+ *
+ * @date       2016-01-15
+ *
+ * @revision   none
+ *
+ * @designer   Eric Tsang
+ *
+ * @programmer Eric Tsang
+ *
+ * @note
+ *
+ * used to wrap an integer of the gmp library so that they will be properly
+ *   initialized and deallocated when an instance of this class is constructed
+ *   or destroyed.
+ */
 #ifndef NUMBER_H
 #define NUMBER_H
 
@@ -6,17 +27,8 @@
 class Number
 {
 public:
-
     Number()
-    {
-        mpz_init(value);
-    }
-
     ~Number()
-    {
-        mpz_clear(value);
-    }
-
     mpz_t value;
 };
 
